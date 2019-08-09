a5e364b1767a5b38141a78c38141e340fd7cbd54
sun <yan.sun@dotcunited.com>
2019-08-09 18:58:00 +0800

    update videox abroad version

---
+++ app/src/main/java/com/dotc/storm/MyApplicaton.java
@@ -0,0 +1,25 @@
+package com.dotc.storm;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.dotc.ll.LocalLog;
+
+public class MyApplicaton extends Application {
+    private static final String TAG = "MyApplication";
+
+    @Override
+    protected void attachBaseContext(Context base) {
+
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+
+}
